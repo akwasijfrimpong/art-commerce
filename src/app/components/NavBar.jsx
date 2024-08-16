916a939340406ef9ba70569a97042375529bbe35
akwasijfrimpong <akwasijfrimpong@gmail.com>
2024-08-16 11:45:32 +1000

    Nav Bar

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+export default function NavBar() {
+  return (
+    <div className="h-[60px] sticky top-0 w-[100%] bg-white z-30 flex gap-10 items-center">
+      <Image src="/logo.svg" alt="logo" width={50} height={50} className="" />
+      <div>
+        <button className="h-[60px] w-[120px] border-[1px] font-bold">
+          {" "}
+          Explore Art
+        </button>
+        <button className="h-[60px] w-[120px] border-[1px] border-l-0 font-bold">
+          Publish Art
+        </button>
+      </div>
+      <input
+        className="rounded-2xl border-[1px] border-black h-[65%] w-[50%] pl-8"
+        placeholder="Search Art"
+      ></input>
+      <div>
+        <button className="h-[60px] w-[120px] border-[1px] font-bold">
+          {" "}
+          Login{" "}
+        </button>
+        <button className="h-[60px] w-[120px] border-[1px] font-bold border-l-0">
+          {" "}
+          Cart{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center relative z-10">
+      <NavBar />
       <section>
         <Hero />
       </section>
