f264f70eef47fd06d3f518ba3344b9b7d12b2185
akwasijfrimpong <akwasijfrimpong@gmail.com>
2024-08-16 21:34:15 +1000

    Fetch requests for both

--- src/app/components/HomeCardOne.jsx
+++ src/app/components/HomeCardOne.jsx
@@ -4,46 +4,98 @@ import { useEffect, useState } from "react";
 
 const HomeCardOne = () => {
   const [listOfVidImages, setListOfVidImages] = useState([]);
+  const [listOfImages, setListOfImages] = useState([]);
+  const [showVid, setShowVid] = useState(true);
 
   useEffect(() => {
-    const popularVids = fetch("https://api.pexels.com/videos/popular", {
-      headers: {
-        Authorization:
-          "ZFec6hZe2wRdP4WN2uHMPwezn5ExJYxpxPieXBqFxumxTRYInrQ4YhkY",
-      },
-    })
-      .then((res) => {
+    const fetchPopularVideos = async () => {
+      try {
+        const res = await fetch("https://api.pexels.com/videos/popular", {
+          headers: {
+            Authorization:
+              "ZFec6hZe2wRdP4WN2uHMPwezn5ExJYxpxPieXBqFxumxTRYInrQ4YhkY",
+          },
+        });
+
         if (!res.ok) {
-          throw new Error("Error Akwasi");
+          throw new Error("Error fetching popular videos");
         }
-        console.log(popularVids, "popularVids");
-        return res.json();
-      })
-      .then((data) => {
-        console.log(JSON.stringify(data.videos), "data");
-        const popularVidImage = data.videos.map((video) => video.image);
+
+        const data = await res.json();
+        const popularVidImage = data.videos.map((video) => {
+          return {
+            image: video.image,
+            id: video.id,
+          };
+        });
         setListOfVidImages(popularVidImage);
-        console.log(popularVidImage, "popularVidImage");
-      })
-      .catch((error) => {
-        console.log("there wwas an errors");
-      });
+      } catch (error) {
+        console.log("There was an error fetching popular videos:", error);
+      }
+    };
+
+    fetchPopularVideos();
+  }, []);
+
+  useEffect(() => {
+    const fetchPopularPhotos = async () => {
+      try {
+        const res = await fetch("https://api.pexels.com/v1/curated", {
+          headers: {
+            Authorization:
+              "ZFec6hZe2wRdP4WN2uHMPwezn5ExJYxpxPieXBqFxumxTRYInrQ4YhkY",
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Error fetching popular photos");
+        }
+
+        const data = await res.json();
+        const popularImage = data.photos.map((photo) => {
+          return { image: photo.src.large, id: photo.id };
+        });
+        setListOfImages(popularImage);
+      } catch (error) {
+        console.log("There was an error fetching popular photos:", error);
+      }
+    };
+
+    fetchPopularPhotos();
   }, []);
 
   return (
-    <div className="h-screen w-screen pt-6  bg-white">
+    <div className="max-h-[900px] w-screen pt-6 bg-white overflow-hidden">
       <div className="flex justify-center gap-6 flex-col items-center">
         <h1 className="font-bold text-[32px]">Explore</h1>
         <div className="flex gap-7">
-          <button className=" bg-black rounded-full text-white w-[150px] h-[40px] border-b-2 border-b-w">
+          <button
+            className={`${
+              showVid
+                ? "bg-black rounded-full text-white w-[150px] h-[40px] border-b-2 border-b-white"
+                : "bg-white border-[2px] w-[150px] h-[40px] text-black rounded-full"
+            }`}
+            onClick={() => {
+              setShowVid(true);
+            }}
+          >
             Popular Videos
-          </button>{" "}
-          <button className="bg-white border-[2px] w-[150px] h-[40px] text-black rounded-full">
-            {" "}
+          </button>
+          <button
+            className={`${
+              !showVid
+                ? "bg-black rounded-full text-white w-[150px] h-[40px] border-b-2 border-b-white"
+                : "bg-white border-[2px] w-[150px] h-[40px] text-black rounded-full"
+            }`}
+            onClick={() => {
+              setShowVid(false);
+            }}
+          >
             Popular Photos
           </button>
         </div>
-        <ProductCard popularVids={listOfVidImages} />
+        {showVid && <ProductCard popularVids={listOfVidImages} />}
+        {!showVid && <ProductCard popularVids={listOfImages} />}
       </div>
     </div>
   );
--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,18 +1,67 @@
 "use client";
+import styles from "./ProductCard.module.css";
 
 import { useEffect } from "react";
 
 export default function ProductCard({ popularVids }) {
+  const tableOne = popularVids.slice(0, 3);
+  const tableTwo = popularVids.slice(3, 8);
+  const tableThree = popularVids.slice(8, 12);
+  const tableFour = popularVids.slice(12, 15);
+
   useEffect(() => {
-    console.log(popularVids, "popularVids");
+    console.log(popularVids.length, "popularVids");
   }, [popularVids]);
   return (
-    <div className="grid grid-rows-3 grid-cols-4 gap-6">
-      {popularVids.map((vid) => {
-        return (
-          <img src={vid} alt="vid" className="rounded-lg w-[300px] h-[auto]" />
-        );
-      })}
+    <div className="flex gap-7">
+      <div className="flex flex-col gap-3">
+        {tableOne.map((vid) => {
+          return (
+            <img
+              src={vid.image}
+              alt="vid"
+              className="flex flex-col w-[320px] rounded"
+              key={vid.id}
+            />
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-3">
+        {tableTwo.map((vid) => {
+          return (
+            <img
+              src={vid.image}
+              alt="vid"
+              className="flex flex-col w-[320px] rounded"
+              key={vid.id}
+            />
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-3">
+        {tableThree.map((vid) => {
+          return (
+            <img
+              src={vid.image}
+              alt="vid"
+              className="flex flex-col w-[320px] rounded"
+              key={vid.id}
+            />
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-3">
+        {tableFour.map((vid) => {
+          return (
+            <img
+              src={vid.image}
+              alt="vid"
+              className="flex flex-col w-[320px] rounded"
+              key={vid.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
